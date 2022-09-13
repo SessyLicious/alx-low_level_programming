0ae027aa7e46afbe04ac10a1bc493441d9ea39a4
Sesilia <sessyxa@yahoo.com>
2022-09-13 13:11:37 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_alphabet - Print the alphabet in lowercase
+ * Description: Print the alphabet in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; leter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
