b5dd6901b6e0bc9314a86961065c2e0ba8b2b963
Sesilia <sessyxa@yahoo.com>
2022-09-13 15:11:53 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * add - add two integers
+ * @a: First input to the function
+ * @b: Second input to the function
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
