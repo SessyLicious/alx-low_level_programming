948ec517b1991ddca9533382bd91176a115af98d
Sesilia <sessyxa@yahoo.com>
2022-09-13 15:03:02 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int r, c, i;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar(48);
+		for (c = 1; c <= 9; c++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			i = r * c;
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((i / 10) + 48);
+			}
+			_putchar((i % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
