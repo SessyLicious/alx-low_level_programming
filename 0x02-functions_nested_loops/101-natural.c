0ef609aaa06695c1700346a4275e2be4282b3c8e
Sesilia <sessyxa@yahoo.com>
2022-09-13 16:07:33 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - check for multiples of 3 and 5
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sum, i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
