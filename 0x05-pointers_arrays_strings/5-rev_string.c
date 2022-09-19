38503fd70c08887d63cd965872706f1f083007e4
Sesilia <sessyxa@yahoo.com>
2022-09-19 07:35:52 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: the string to be reversed.
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len, j, i;
+	char first, last;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	j = length - 1;
+	i = 0;
+
+	while (j > i)
+	{
+		first = s[i];
+		last = s[j];
+		s[i] = last;
+		s[j] = first;
+		j--;
+		i++;
+	}
+}
