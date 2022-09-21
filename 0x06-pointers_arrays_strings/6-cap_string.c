eb3dddf256147b99b10231ac4036385d01fa940b
Sesilia <sessyxa@yahoo.com>
2022-09-21 10:02:28 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @x: string to use
+ * Return: string
+ */
+char *cap_string(char *x)
+{
+	char plc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i = 13, j = 0, k;
+
+	while (x[j])
+	{
+		k = 0;
+		while (k < j)
+		{
+			if ((j == 0 || x[j - 1] == plc[k]) && (x[j] >= 97 && x[j] <= 122))
+			{
+				x[j] = x[j] - 32;
+			}
+			k++;
+		}
+		j++;
+	}
+	return (x);
+}
+
