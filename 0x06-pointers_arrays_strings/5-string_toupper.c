23fbbfac8fb7cc87920c993a98648c27d4d6215d
Sesilia <sessyxa@yahoo.com>
2022-09-21 09:34:26 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: input string
+ * Return: the pointer to dest
+ */
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (x[count])
+	{
+		if (x[count] >= 97 && x[count] <= 122)
+		{
+			x[count] = x[count] - 32;
+		}
+		count++;
+	}
+	return (x);
+}
