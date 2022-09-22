eef934d4a852131ae76080df208d23dfd27ece86
Sesilia <sessyxa@yahoo.com>
2022-09-22 06:09:21 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,7 +5,25 @@
  * @x: string to use
  * Return: string
  */
-char *cap_string(char *str)
+char *cap_string(char *x)
 {
+	char plc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i = 13;
+	int j = 0, k;
 
-	
+	while (x[j])
+	{
+		k = 0;
+		while (k < i)
+		{
+			if ((j == 0 || x[j - 1] == plc[k]) && (x[j] >= 97 && x[j] <= 122))
+			{
+				x[j] = x[j] - 32;
+			}
+			k++;
+		}
+		j++;
+	}
+	return (x);
+}
+
