7df5a7aa540ebd3a7cdb35a983c3c3f14dd1b5cc
Sesilia <sessyxa@yahoo.com>
2022-10-06 14:14:17 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,44 +14,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 
 	if (s1 == NULL)
-	{
 		i = 0;
-	}
 	else
 	{
 		for (i = 0; s1[i]; i++)
-		{
 			;
-		}
 	}
 	if (s2 == NULL)
-	{
 		j = 0;
-	}
 	else
 	{
 		for (j = 0; s2[j]; j++)
-		{
 			;
-		}
 	}
 	if (j > n)
-	{
 		j = n;
-	}
 	str = malloc(sizeof(char) * (i + j + 1));
 	if (str == NULL)
-	{
 		return (NULL);
-	}
 	for (k = 0; k < i; k++)
-	{
 		str[k] = s1[k];
-	}
 	for (k = 0; k < j; k++)
-	{
 		str[k + i] = s2[k];
-	}
 	str[i + j] = '\0';
 	return (str);
 }
