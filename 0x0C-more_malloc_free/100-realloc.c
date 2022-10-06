23e569d56dea934a0d0b072a2aac756f59fdd9db
Sesilia <sessyxa@yahoo.com>
2022-10-06 13:52:08 -0700

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,11 +15,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *oldp = ptr;
 
 	if (ptr == NULL)
+	{
 		p = malloc(new_size);
 		return (p);
+	}
 	else if (new_size == 0)
+	{
 		free(ptr);
 		return (NULL);
+	}
 	else if (new_size == old_size)
 		return (ptr);
 	p = malloc(new_size);
