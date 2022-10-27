ab218264ff43322bb081a757631b1e73ba3c58ec
Sesilia <sessyxa@yahoo.com>
2022-10-27 13:14:33 -0700

    All tasks- good luck

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int base = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		base = 2 * base + (b[i] - '0');
+	}
+	return (base);
+}
